88de16bdba4b177e6236367f180995fde01d7df8
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2020-03-15 18:45:45 -0500

    JZ 3/15/2020: Main Page and About Page Frames created

--- app/src/main/java/com/example/guesswhoaini/AboutActivity.java
+++ app/src/main/java/com/example/guesswhoaini/AboutActivity.java
@@ -1,22 +1,24 @@
 package com.example.guesswhoaini;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about);
     }
 
-    public void aboutFunction(View view)
+    public void returnFunction(View view)
     {
-        Intent intent = new Intent(this,AboutActivity.class);
+        Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
     }
+
+
 }
