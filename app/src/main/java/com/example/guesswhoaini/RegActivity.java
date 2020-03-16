a3063d8a2e9d81e1b2908d2ce5f665a5ea225376
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2020-03-16 14:39:49 -0500

    JZ 3/16/2020: Reg activity created and half implemented. Firebase Struct ready to use. Encountered problems with Firebase Analytics. Problem Solved. !!Rebuild and Sync Gradle!!

--- app/src/main/java/com/example/guesswhoaini/RegActivity.java
+++ app/src/main/java/com/example/guesswhoaini/RegActivity.java
@@ -6,23 +6,23 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class IntloginActivity extends AppCompatActivity {
+public class RegActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intlogin);
+        setContentView(R.layout.activity_reg);
     }
 
+    public void logFunction(View view)
+    {
+        Intent intent = new Intent(this,IntloginActivity.class);
+        startActivity(intent);
+    }
 
     public void returnFunction(View view)
     {
         Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void regFunction(View view)
-    {
-        Intent intent = new Intent(this,RegActivity.class);
         startActivity(intent);
     }
 }
