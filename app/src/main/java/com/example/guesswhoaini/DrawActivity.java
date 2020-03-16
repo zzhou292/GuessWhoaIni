2012b0cc59b9ea02a0056d1c54386dd12fa96bb3
laimaster21 <cyuan42@wisc.edu>
2020-03-16 16:06:35 -0500

    Working on the drawing activity, found a demo for fingerpainting

---
+++ app/src/main/java/com/example/guesswhoaini/DrawActivity.java
@@ -0,0 +1,15 @@
+package com.example.guesswhoaini;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrawActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ethlogin);
+    }
+//https://stackoverflow.com/questions/24164334/where-to-find-android-fingerpaint-demo-android-studio-era
+    // found a finger print demo from google, TODO
+
+}
