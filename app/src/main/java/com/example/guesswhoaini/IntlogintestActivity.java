9fae3171377bf75491694b765b2554c4ba00489f
zzhou292 <38740063+zzhou292@users.noreply.github.com>
2020-03-16 16:17:14 -0500

    JZ 3/16/2020: Firebase Registration func implemented. Com from app to Firebase server established. After Hitting "Sign-Up" Button in RegActivity, data will be sent to Firebase server and the app is going switch activity to IntlogintestActivity

---
+++ app/src/main/java/com/example/guesswhoaini/IntlogintestActivity.java
@@ -0,0 +1,18 @@
+package com.example.guesswhoaini;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntlogintestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intlogintest);
+    }
+
+
+}
