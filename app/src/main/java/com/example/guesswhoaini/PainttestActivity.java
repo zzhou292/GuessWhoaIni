51f71cb1033fafd581096e05f4146d9099c8dccc
zzhou292 <zzhou292@wisc.edu>
2020-03-18 12:54:20 -0500

    JZ 3/18/2020:A Painting demo has been successfully implemented. Found the func by clicking "PAINT TEST" button on the main page.

--- app/src/main/java/com/example/guesswhoaini/PainttestActivity.java
+++ app/src/main/java/com/example/guesswhoaini/PainttestActivity.java
@@ -16,7 +16,7 @@ public class PainttestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_painttest);
 
         paintView = (PaintView) findViewById(R.id.paintView);
         DisplayMetrics metrics = new DisplayMetrics();
