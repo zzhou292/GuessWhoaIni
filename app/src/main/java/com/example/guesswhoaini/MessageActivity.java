e28e4fa7a11b4fe53ec1378f0df6c650ac7f84dd
zzhou292 <zzhou292@wisc.edu>
2020-04-04 01:40:11 -0500

    JZ 4/4/2020: Chat room partially implemented, messages can be synced to different devices.
    Pending: username display, sign off function of the software, auto scroll down

--- app/src/main/java/com/example/guesswhoaini/MessageActivity.java
+++ app/src/main/java/com/example/guesswhoaini/MessageActivity.java
@@ -2,25 +2,32 @@ package com.example.guesswhoaini;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
+
+
 public class MessageActivity extends AppCompatActivity {
+    private FirebaseListAdapter<ChatMessage> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message1);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-
+        displayChatMessages();
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,5 +48,30 @@ public class MessageActivity extends AppCompatActivity {
                 input.setText("");
             }
         });
+    }
+
+    private void displayChatMessages() {
+        ListView listOfMessages = (ListView)findViewById(R.id.list_of_messages);
+
+        adapter = new FirebaseListAdapter<ChatMessage>(this, ChatMessage.class,
+                R.layout.message, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, ChatMessage model, int position) {
+                // Get references to the views of message.xml
+                TextView messageText = (TextView)v.findViewById(R.id.message_text);
+                TextView messageUser = (TextView)v.findViewById(R.id.message_user);
+                TextView messageTime = (TextView)v.findViewById(R.id.message_time);
+
+                // Set their text
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+                // Format the date before showing it
+                messageTime.setText(DateFormat.format("dd-MM-yyyy (HH:mm:ss)",
+                        model.getMessageTime()));
+            }
+        };
+
+        listOfMessages.setAdapter(adapter);
     }
 }
