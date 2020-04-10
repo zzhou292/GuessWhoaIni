dc4ef9dae9cab47c8d3272ef9acfd7e844e9c732
laimaster21 <cyuan42@wisc.edu>
2020-04-10 01:32:04 -0500

    some starting code for blue tooth

--- app/src/main/java/com/example/guesswhoaini/BtlogintestActivity.java
+++ app/src/main/java/com/example/guesswhoaini/BtlogintestActivity.java
@@ -1,12 +1,22 @@
 package com.example.guesswhoaini;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Set;
+
 public class BtlogintestActivity extends AppCompatActivity {
+    // use bluetooth adapter to connect other devices
+    // following tutorial from https://www.tutorialspoint.com/android/android_bluetooth.htm
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +26,10 @@ public class BtlogintestActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
         textView2.setText("Hello "+str);
+
+        // initialize bluetooth adapter
+        BA = BluetoothAdapter.getDefaultAdapter();
+        lv = (ListView)findViewById(R.id.listview);
     }
 
 
